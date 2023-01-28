1552efafbc4303fd90e337e380c8a7e979ee367e
FaheemRafiq <faheemkhan198742@gmail.com>
2023-01-28 12:24:34 +0500

    Controller CURD

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Validation\ValidationException;
 
 class UserController extends Controller
 {
@@ -23,6 +25,17 @@ class UserController extends Controller
 
     public function update(Request $request, $id)
     {
+        try {
+            $validatedData = $request->validate([
+                'name' => 'required|string|min:6|max:255|unique:users,name,'.$id,
+                'email' => 'required|string|email|min:10|max:255|unique:users,email,'.$id,
+            ]);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'error' => $e->getMessage(),
+                'errors' => $e->errors(),
+            ], 422);
+        }
         $user = User::find($id);
         
         $user->name = $request->name;
@@ -33,12 +46,32 @@ class UserController extends Controller
 
     public function store(Request $request)
     {
+        try {
+            $validatedData = $request->validate([
+                'name' => 'required|string|min:6|max:255|unique:users',
+                'email' => 'required|string|email|min:10|max:255|unique:users',
+                'password' => 'required|string|min:6',
+            ]);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'error' => $e->getMessage(),
+                'errors' => $e->errors(),
+            ], 422);
+        }
+
         $user = new User();
         $user->name = $request->name;
         $user->email = $request->email;
-        $user->password = $request->password;
+        $user->password = bcrypt($request->password);
+        $user->remember_token = Str::random(10);
         $user->save();
-        return response()->json('User created successfully');
+        return response()->json([
+            'status' => 200,
+            'message' => 'User created successfully',
+            'name' => $user->name,
+            'email' => $user->email,
+        ]);
+        
     }
 
 }
